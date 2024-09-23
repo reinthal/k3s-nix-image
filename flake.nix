24f3ffbac5f395fc69e4473ccdcea48dae44bc40
Alexander Reinthal <email@reinthal.me>
2024-09-23 13:25:09 +0200

    add lxc

--- flake.nix
+++ flake.nix
@@ -7,19 +7,37 @@
     };
   };
   outputs = {
-    self,
     nixpkgs,
     nixos-generators,
     ...
-  }: {
+  }: let
+    system = "x86_64-linux";
+  in {
     packages.x86_64-linux = {
       proxmox = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
+        system = "${system}";
+        specialArgs = {
+          pkgs = nixpkgs;
+          diskSize = 20 * 1024;
+        };
         modules = [
-          ({ ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
+          ({...}: {nix.registry.nixpkgs.flake = nixpkgs;})
           ./server
         ];
         format = "proxmox";
+      };
+
+      proxmox_lxc = nixos-generators.nixosGenerate {
+        system = "${system}";
+        specialArgs = {
+          pkgs = nixpkgs;
+          diskSize = 20 * 1024;
+        };
+        modules = [
+          ({...}: {nix.registry.nixpkgs.flake = nixpkgs;})
+          ./server
+        ];
+        format = "proxmox-lxc";
       };
     };
   };
